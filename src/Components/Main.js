e30c81e43d726a718353bec3efc910dcd0462ea0
Joseph Price <josephprice747@gmail.com>
2023-03-01 16:25:32 +0000

    add data.json, add data.map to Main so the json array data is used

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,19 +1,19 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
+import data from "../data.json";
 
 export default function Main() {
   return (
     <main>
-      <HornedBeast
-        title="Smaug"
-        imageUrl="https://qph.cf2.quoracdn.net/main-qimg-d7cceaac6251d5e158e25165e380e606-lq"
-        description="Smaug be stealin"
-      />
-      <HornedBeast
-        title="Rhino"
-        imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4cMNXrgI3kVcdIYiFa8lBZKkFDikdTgm_oDUuJ9n6eiRTxSRHOEJcDjIi2F3HrI1tPzM&usqp=CAU"
-        description="Endangered boi lookin' suave"
-      />
+      {data.map((beast) => {
+        return (
+          <HornedBeast
+            title={beast.title}
+            imageUrl={beast.image_url}
+            description={beast.description}
+          />
+        );
+      })}
     </main>
   );
 }
