fc8646c3aef6c7ece39a73493d30470f2956e1d3
Joseph Price <josephprice747@gmail.com>
2023-03-03 11:19:51 +0000

    starting to add form to app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,30 @@ import { useState } from "react";
 import SelectedBeast from "./Components/SelectedBeast";
 
 function App() {
+  const [formData, setFormData] = useState ({
+    userName: "",
+    colour: "",
+    drink: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleChange(event) {
+    let newFormData = ( ...formData, [event.target.name]: event.target.value) ;
+    setFormData(newFormData);
+  }
+
+  return (
+    <div className="App">
+      <h1>Form Demo</h1>
+      <form onSubmit={handleSubmit}>
+        <input name="userName" colour="colour" drink="drink" />
+      </form>
+    </div>
+  )
+
   const [modalShow, setModalShow] = useState(false);
   const [theBeast, setTheBeast] = useState({});
 
